07fdb83fec2902fa350f084c7db329ac446127e3
Celso Dantas <celsodantas@gmail.com>
2013-02-25 22:55:08 -0600

    disabling bash_completion temporarily

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ source $HOME/.bash/scripts/alias.sh
 source $HOME/.bash/scripts/options.sh
 
 source $HOME/.bash/scripts/git_completion.sh
-source $HOME/.bash/scripts/bash_completion.sh
+# bash completion temporarily disabled
+#source $HOME/.bash/scripts/bash_completion.sh
 source $HOME/.bash/scripts/gem_completion.sh
 source $HOME/.bash/scripts/utils.sh
 
