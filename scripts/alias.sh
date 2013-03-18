720525cd5260fd9cece00df7e6c956b230ed3cba
Celso Dantas <celsodantas@gmail.com>
2013-03-18 17:41:34 -0500

    adding bundle exec alias "be"

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -1,5 +1,6 @@
 alias gl="git log --graph --pretty='format:%C(yellow)%h%C(cyan)%d%Creset %s => %C(green)%an%C(white), %C(red)%ar%Creset'"
 alias gs="git status"
+alias be="bundle exec"
 alias cleanup="sudo rm -rf /private/var/log/asl/*"
 alias growl="growlnotify"
 alias js="java -jar $HOME/Library/Java/Extensions/js.jar -opt -1"
